8560d5daa6229e14b88edf41d5cd1e902197365e
John Robinson <johnsjobs7@gmail.com>
2025-02-21 23:28:24 +0000

    Update LoginAuthKeysTest.php

--- tests/Feature/LoginAuthKeysTest.php
+++ tests/Feature/LoginAuthKeysTest.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Str;
 uses(RefreshDatabase::class);
 
 it('can create and delete a login auth key', function () {
-    // Create a new login auth key record.
+    // Create a new login auth key value pair record.
     $loginAuthKey = LoginAuthKey::create([
         'key_01' => Str::uuid(),
         'key_02' => Str::uuid(),
