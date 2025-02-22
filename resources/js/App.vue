94c91aa8aa450f471a13539afa48cba00ecce0eb
John Robinson <johnsjobs7@gmail.com>
2025-02-22 00:09:13 +0000

    vite and vue3.js added

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <h1>Hello, Vue 3 with Vite!</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'App',
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #57033f;
+  }
+  </style>
+  
