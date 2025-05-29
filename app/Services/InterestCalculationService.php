f51ea4e9377bace107510de9cd1be48d3808e697
John Robinson <johnsjobs7@gmail.com>
2025-05-29 15:53:48 +0100

    Update InterestCalculationService.php

--- app/Services/InterestCalculationService.php
+++ app/Services/InterestCalculationService.php
@@ -11,25 +11,14 @@ class InterestCalculationService
         $totalInterest = $monthlyInterest * $months;
         $totalRepayment = $amount + $totalInterest;
 
-        $monthlyBreakdown = [];
-
-        for ($i = 1; $i <= $months; $i++) {
-            $monthlyBreakdown[] = [
-                'month' => $i,
-                'interest' => round($monthlyInterest, 2),
-                'total_due' => round($amount + $monthlyInterest * $i, 2),
-            ];
-        }
-
         return [
-            'principal'        => round($amount, 2),
-            'annual_rate'      => round($interest, 2),
-            'monthly_rate'     => round($monthlyRate * 100, 2),
-            'months'           => $months,
-            'monthly_interest' => round($monthlyInterest, 2),
-            'total_interest'   => round($totalInterest, 2),
-            'total_repayment'  => round($totalRepayment, 2),
-            'breakdown'        => $monthlyBreakdown,
+            'principal'         => round($amount, 2),
+            'annual_rate'       => round($interest, 2),
+            'months'            => $months,
+            'monthly_interest'  => round($monthlyInterest, 2),
+            'total_interest'    => round($totalInterest, 2),
+            'monthly_repayment' => round($totalRepayment / $months, 2),
+            'total_repayment'   => round($totalRepayment, 2),
         ];
     }
 }
