4f79f08f18d2292d84f5a90f168d42525ec7c101
John <johnsjobs7@gmail.com>
2025-06-06 14:32:52 +0100

    Update OneHundredAPIs.vue

--- resources/js/components/OneHundredAPIs.vue
+++ resources/js/components/OneHundredAPIs.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex flex-col justify-center min-w-full min-h-full px-6 py-6 lg:px-8">
-        <h1 class="flex flex-col justify-center min-w-full pt-8">{{ apiEndpoints.length }} out of 100 API Endpoints</h1>
+        <h1 class="flex flex-col justify-center min-w-full pt-8">{{ apiEndpoints.length - 1 }} out of 100 API Endpoints</h1>
         <dropdown></dropdown>
         <api-description v-show="store.showDescription"></api-description>
         <ms-to-min-conversion v-show="store.showMsToMin"></ms-to-min-conversion>
