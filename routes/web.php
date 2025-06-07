dca0525a13b4cb02bbea9123800cfce2bc03f85b
John Robinson <johnsjobs7@gmail.com>
2025-06-07 12:37:54 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OneHundredApisController;
 
 // # View #
+Route::get('/', [OneHundredApisController::class, 'home'])->middleware('throttle:heavy');
 Route::get('/one-hundred-apis', [OneHundredApisController::class, 'home'])->middleware('throttle:heavy');
 
 Route::get('/petrol-mileage', function () {
