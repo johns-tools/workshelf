2010efab9d29110a98f65494880da5559e87297a
johns-tools <147048594+johns-tools@users.noreply.github.com>
2025-06-07 12:37:33 +0100

    Add Google Analytics snippet

--- resources/views/ms_min_conversion.blade.php
+++ resources/views/ms_min_conversion.blade.php
@@ -4,6 +4,15 @@
     <title>MS to Min conversion</title>
     @vite('resources/css/app.css')
     @vite('resources/js/app.js')
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-R8QDSQRD0Z"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-R8QDSQRD0Z');
+    </script>
 </head>
     <body class="h-full bg-gray-900 text-white">
         <div id="app" class="max-w-[750px] mx-auto">
